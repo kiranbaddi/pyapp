c562b863b60cbbdcbe2c7bda55c912fce72126db
Bhargavi9521 <ballinabhargavi2001@gmail.com>
2026-04-22 20:25:59 +0530

    Modified API call-openai

--- call-openai.py
+++ call-openai.py
@@ -1,4 +1,14 @@
-import connect-openai
+import os
+from openai import OpenAI
 
-if __name__ == "__main__":
-    connect-openai.main
+client = OpenAI(
+    api_key=os.environ.get("OPENAI_API_KEY"),
+)
+
+response = client.responses.create(
+    model="gpt-5.2",
+    instructions="You are a coding assistant that talks like a pirate.",
+    input="How do I check if a Python object is an instance of a class?",
+)
+
+print(response.output_text)
